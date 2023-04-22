dbc2f6eda220a34c172c3674a8954405e928bf49
Prof Nilson Mori <nilsonmori@yahoo.com>
2023-04-21 21:35:19 -0300

    ChonOS Repository Creator

---
+++ repositoy_generator.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+SRC="src/"
+DPKG="public_html/chonos/"
+
+getLatest(){
+    PACKAGE=$1
+    LATEST_RELEASE_URL=$2
+    FILENAME=$(echo "$LATEST_RELEASE_URL" | cut -d'/' -f $(echo $(echo $LATEST_RELEASE_URL | grep -o '/' | wc -l)+1 | bc))
+    GITREPO=$(echo $LATEST_RELEASE_URL | cut -d "/" -f 5)
+    RELEASE=`echo $FILENAME | cut -d "." -f 1`
+
+    if [ ! -d $SRC ]; then
+        mkdir $SRC
+    elif [ -d $SRC$GITREPO-$RELEASE ]; then
+        rm -rf $SRC$GITREPO-$RELEASE
+    fi
+
+    if [ ! -f $SRC$FILENAME ]; then
+        wget -P $SRC $LATEST_RELEASE_URL
+    else
+        local_modified_date=$(date -d "$(date -r $SRC$FILENAME '+%Y-%m-%d %H:%M:%S')" +%s)
+        remote_release_date=$(date -d "$(curl -s https://api.github.com/repos/chongroup/$GITREPO/releases/latest | grep "published_at" | head -n 1 | awk -F': ' '{print $2}' | tr -d '",')" +%s)
+        if [ "$remote_release_date" -gt "$local_modified_date" ]; then
+            wget -P $SRC $LATEST_RELEASE_URL
+        fi
+    fi
+    tar -xzf $SRC$FILENAME -C $SRC
+   
+    VERSION=`egrep "Version" $SRC/$GITREPO-$RELEASE/DEBIAN/control | cut -d ":" -f 2 | xargs`
+    mkdir -p $DPKG$PACKAGE
+    dpkg-deb -b $SRC$GITREPO-$RELEASE/ $DPKG$PACKAGE/$PACKAGE-$VERSION.deb
+}
+
+
+getLatest "chonos-ddnsmng" "https://github.com/chongroup/dpkg-chonos-ddnsmng/archive/refs/tags/ddnsmng-latest.tar.gz"
+
+cd public_html
+dpkg-scanpackages chonos/ /dev/null | gzip -9c > Packages.gz
+dpkg-scanpackages chonos/ /dev/null > Release

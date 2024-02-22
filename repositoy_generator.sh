cd9e6ad7e2bc9c49bcc8773273b59add4c223911
Nilson Lazarin <nilsonmori@yahoo.com>
2024-02-22 16:26:05 -0300

    Discovery the tagName of latest release

--- repositoy_generator.sh
+++ repositoy_generator.sh
@@ -4,7 +4,14 @@ DPKG="public_html/chonos/"
 
 getLatest(){
     PACKAGE=$1
-    LATEST_RELEASE_URL=$2
+    GITNAME=$2
+    LATEST_INFORMATION="https://api.github.com/repos/chon-group/$2/releases/latest"
+    echo "Searching for $LATEST_INFORMATION"
+    curl -s $LATEST_INFORMATION > /tmp/$PACKAGE.latest
+
+    TAG_NAME=$(cat /tmp/$PACKAGE.latest | grep "tag_name" | head -n 1 | awk -F'["]' '{print $4}')
+    
+    LATEST_RELEASE_URL="https://github.com/chon-group/$GITNAME/archive/refs/tags/$TAG_NAME.tar.gz"
     FILENAME=$(echo "$LATEST_RELEASE_URL" | rev | cut -d "/" -f 1 | rev)
     GITREPO=$(echo $LATEST_RELEASE_URL | cut -d "/" -f 5)
     RELEASE=`echo $FILENAME | cut -d "." -f 1`
@@ -19,7 +26,7 @@ getLatest(){
         wget -P $SRC $LATEST_RELEASE_URL
     else
         local_modified_date=$(date -d "$(date -r $SRC$FILENAME '+%Y-%m-%d %H:%M:%S')" +%s)
-        remote_release_date=$(date -d "$(curl -s https://api.github.com/repos/chon-group/$GITREPO/releases/latest | grep "published_at" | head -n 1 | awk -F': ' '{print $2}' | tr -d '",')" +%s)
+        remote_release_date=$(date -d "$(cat /tmp/$PACKAGE.latest | grep "published_at" | head -n 1 | awk -F': ' '{print $2}' | tr -d '",')" +%s)
         if [ "$remote_release_date" -gt "$local_modified_date" ]; then
             rm $SRC$FILENAME
             wget -P $SRC $LATEST_RELEASE_URL
@@ -28,48 +35,37 @@ getLatest(){
     tar -xzf $SRC$FILENAME -C $SRC
    
     VERSION=`egrep "Version:" $SRC/$GITREPO-$RELEASE/DEBIAN/control | cut -d ":" -f 2 | xargs`
-    mkdir -p $DPKG$PACKAGE
+    mkdir -p $DPKG
     rm $SRC$GITREPO-$RELEASE/*.md 2> /dev/null
     rm $SRC$GITREPO-$RELEASE/LICENCE 2> /dev/null
-    dpkg-deb -b $SRC$GITREPO-$RELEASE/ $DPKG$PACKAGE/$PACKAGE-$VERSION.deb
+    dpkg-deb -b $SRC$GITREPO-$RELEASE/ $DPKG$PACKAGE-$VERSION.deb
 }
 
 
-getLatest "chonos-ddnsmng"              "https://github.com/chon-group/dpkg-chonos-ddnsmng/archive/refs/tags/ddnsmng-latest.tar.gz"
-getLatest "chonos-log"                  "https://github.com/chon-group/dpkg-chonos-log/archive/refs/tags/log-latest.tar.gz"
-getLatest "chonos-task"                 "https://github.com/chon-group/dpkg-chonos-task/archive/refs/tags/task-latest.tar.gz"
-getLatest "javino"                      "https://github.com/chon-group/dpkg-javino/archive/refs/tags/javino-latest.tar.gz"
-getLatest "chonos-embeddedmas"          "https://github.com/chon-group/dpkg-chonos-embeddedmas/archive/refs/tags/embeddedmas-latest.tar.gz"
-getLatest "chonos-firmwaremng"          "https://github.com/chon-group/dpkg-chonos-firmwaremng/archive/refs/tags/firmwaremng-latest.tar.gz"
-getLatest "chonos-sysconfig"            "https://github.com/chon-group/dpkg-chonos-sysconfig/archive/refs/tags/sysconfig-latest.tar.gz"
-getLatest "chonide"                     "https://github.com/chon-group/dpkg-chonide/archive/refs/tags/chonide-latest.tar.gz"
-getLatest "jason-cli"                   "https://github.com/chon-group/dpkg-jason/archive/refs/tags/jasoncli-latest.tar.gz"
-getLatest "jacamo-cli"                  "https://github.com/chon-group/dpkg-jacamo/archive/refs/tags/jacamocli-latest.tar.gz"
-getLatest "chonos-serial-port-emulator" "https://github.com/chon-group/dpkg-virtualport-driver/archive/refs/tags/virtualbot-latest.tar.gz"
-getLatest "chonos-network"              "https://github.com/chon-group/dpkg-chonos-network/archive/refs/tags/network-latest.tar.gz"
-getLatest "chonos"                      "https://github.com/chon-group/dkpg-chonos/archive/refs/tags/chonos-latest.tar.gz"
-getLatest "chonos-neighbors"            "https://github.com/chon-group/dpkg-chonos-neighbors/archive/refs/tags/neighbors-latest.tar.gz"
-getLatest "chonos-cert"                 "https://github.com/chon-group/dpkg-chonos-cert/archive/refs/tags/cert-latest.tar.gz"
-getLatest "contextnetserver-latest"     "https://github.com/chon-group/contextNetServer/archive/refs/tags/contextnetserver-latest.tar.gz"
-getLatest "chonos-simulide"             "https://github.com/chon-group/dpkg-simulide/archive/refs/tags/simulide-latest.tar.gz"
+#getLatest DEBIAN-PACKAGE-NAME   GITHUB-REPO-NAME
+getLatest "chonos-ddnsmng" "dpkg-chonos-ddnsmng"
+getLatest "chonos-log" "dpkg-chonos-log"
+getLatest "chonos-task" "dpkg-chonos-task"
+getLatest "javino" "dpkg-javino"
+getLatest "chonos-embeddedmas" "dpkg-chonos-embeddedmas"
+getLatest "chonos-firmwaremng" "dpkg-chonos-firmwaremng"
+getLatest "chonos-sysconfig" "dpkg-chonos-sysconfig"
+getLatest "chonide" "dpkg-chonide"
+getLatest "jason-cli" "dpkg-jason"
+getLatest "jacamo-cli" "dpkg-jacamo"
+getLatest "chonos-serial-port-emulator" "dpkg-virtualport-driver"
+getLatest "chonos-network" "dpkg-chonos-network"
+getLatest "chonos" "dkpg-chonos"
+getLatest "chonos-neighbors" "dpkg-chonos-neighbors"
+getLatest "chonos-cert" "dpkg-chonos-cert"
+getLatest "contextnetserver-latest" "contextNetServer"
+getLatest "chonos-simulide" "dpkg-simulide"
 
-mkdir -p public_html/dists/chonos/main/binary-i386
-
+mkdir -p public_html/dists/chonos/main/binary-all
 cd public_html
 
-dpkg-scanpackages chonos/ /dev/null | gzip -9c > dists/chonos/main/binary-i386/Packages.gz
-dpkg-scanpackages chonos/ /dev/null > dists/chonos/main/binary-i386/Release
-
-mkdir -p dists/chonos/main/binary-all
-cp dists/chonos/main/binary-i386/* dists/chonos/main/binary-all/
-
-mkdir -p dists/chonos/main/binary-amd64
-cp dists/chonos/main/binary-i386/* dists/chonos/main/binary-amd64/
-
-mkdir -p dists/chonos/main/binary-arm64
-cp dists/chonos/main/binary-i386/* dists/chonos/main/binary-arm64/
-
-mkdir -p dists/chonos/main/binary-armhf
-cp dists/chonos/main/binary-i386/* dists/chonos/main/binary-armhf/
+dpkg-scanpackages chonos/ > dists/chonos/main/binary-all/Release
+cat dists/chonos/main/binary-all/Release | gzip -9c > dists/chonos/main/binary-all/Packages.gz
 
 apt-ftparchive release . > Release
+cd ../
